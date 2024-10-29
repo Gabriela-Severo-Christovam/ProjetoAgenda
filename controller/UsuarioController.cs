8f979264487c11a53d3d7049c460d01e63f061d0
Gabriela Severo <severogabriela4@gmail.com>
2024-10-29 11:31:14 -0300

    add usuario controller

---
+++ controller/UsuarioController.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using ProjetoAgenda.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAgenda.controller
+{
+    internal class UsuarioController
+    {
+        public bool AddUsuario(string nome, string usuario, string telefone, string senha)
+        {
+            MySqlConnection conexao = ConexaoDB.Criarconexao();
+ 
+        }
+    }
+}
