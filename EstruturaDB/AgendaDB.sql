10c29c9f6593ec3de371e7d5d14a0e3a786a1f45
Gabriela Severo <severogabriela4@gmail.com>
2024-10-29 10:13:41 -0300

    Add database dbAgenda

---
+++ EstruturaDB/AgendaDB.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE dbAgenda;
+USE dbAgenda;
+
+CREATE TABLE tbUsuarios(
+	nome VARCHAR(80) NOT NULL,
+    usuario VARCHAR(40) PRIMARY KEY,
+    telefone VARCHAR(15),
+    senha VARCHAR(8) NOT NULL
+    );
+    
+    select * from cadastro; 
+    
+    
