63a24e209864ca2c57d3078f87a579b476242170
Gabriela <severogabriela4@gmail.com>
2024-11-26 08:13:57 -0300

    alterando o desing

--- Views/Frmcategoria.Designer.cs
+++ Views/Frmcategoria.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
+            btn_alterar = new Button();
+            btn_excluir = new Button();
             label2 = new Label();
             btn_cadastrar = new Button();
             txtbox_categoria = new TextBox();
             dgvCategoria = new DataGridView();
-            btn_excluir = new Button();
-            btn_alterar = new Button();
             groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgvCategoria).BeginInit();
             SuspendLayout();
@@ -47,26 +47,51 @@
             groupBox1.Controls.Add(btn_cadastrar);
             groupBox1.Controls.Add(txtbox_categoria);
             groupBox1.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            groupBox1.Location = new Point(44, 30);
+            groupBox1.Location = new Point(50, 40);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(270, 266);
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(309, 355);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Insira nova categoria";
             // 
+            // btn_alterar
+            // 
+            btn_alterar.Location = new Point(9, 281);
+            btn_alterar.Margin = new Padding(2, 3, 2, 3);
+            btn_alterar.Name = "btn_alterar";
+            btn_alterar.Size = new Size(293, 61);
+            btn_alterar.TabIndex = 5;
+            btn_alterar.Text = "Alterar";
+            btn_alterar.UseVisualStyleBackColor = true;
+            btn_alterar.Click += btn_alterar_Click;
+            // 
+            // btn_excluir
+            // 
+            btn_excluir.Location = new Point(9, 204);
+            btn_excluir.Margin = new Padding(2, 3, 2, 3);
+            btn_excluir.Name = "btn_excluir";
+            btn_excluir.Size = new Size(293, 61);
+            btn_excluir.TabIndex = 2;
+            btn_excluir.Text = "Excluir";
+            btn_excluir.UseVisualStyleBackColor = true;
+            btn_excluir.Click += btn_excluir_Click;
+            // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(8, 31);
+            label2.Location = new Point(9, 41);
             label2.Name = "label2";
-            label2.Size = new Size(0, 17);
+            label2.Size = new Size(0, 23);
             label2.TabIndex = 4;
             // 
             // btn_cadastrar
             // 
-            btn_cadastrar.Location = new Point(7, 93);
+            btn_cadastrar.Location = new Point(8, 124);
+            btn_cadastrar.Margin = new Padding(3, 4, 3, 4);
             btn_cadastrar.Name = "btn_cadastrar";
-            btn_cadastrar.Size = new Size(257, 47);
+            btn_cadastrar.Size = new Size(294, 63);
             btn_cadastrar.TabIndex = 1;
             btn_cadastrar.Text = "&Cadastrar";
             btn_cadastrar.UseVisualStyleBackColor = true;
@@ -75,51 +100,32 @@
             // txtbox_categoria
             // 
             txtbox_categoria.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txtbox_categoria.Location = new Point(7, 45);
+            txtbox_categoria.Location = new Point(8, 60);
+            txtbox_categoria.Margin = new Padding(3, 4, 3, 4);
             txtbox_categoria.Name = "txtbox_categoria";
-            txtbox_categoria.Size = new Size(257, 33);
+            txtbox_categoria.Size = new Size(293, 39);
             txtbox_categoria.TabIndex = 0;
             // 
             // dgvCategoria
             // 
             dgvCategoria.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvCategoria.Location = new Point(331, 8);
+            dgvCategoria.Location = new Point(378, 11);
+            dgvCategoria.Margin = new Padding(3, 4, 3, 4);
             dgvCategoria.Name = "dgvCategoria";
             dgvCategoria.RowHeadersWidth = 62;
             dgvCategoria.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvCategoria.Size = new Size(517, 339);
+            dgvCategoria.Size = new Size(338, 455);
             dgvCategoria.TabIndex = 1;
-            // 
-            // btn_excluir
-            // 
-            btn_excluir.Location = new Point(8, 153);
-            btn_excluir.Margin = new Padding(2);
-            btn_excluir.Name = "btn_excluir";
-            btn_excluir.Size = new Size(256, 46);
-            btn_excluir.TabIndex = 2;
-            btn_excluir.Text = "Excluir";
-            btn_excluir.UseVisualStyleBackColor = true;
-            btn_excluir.Click += btn_excluir_Click;
-            // 
-            // btn_alterar
-            // 
-            btn_alterar.Location = new Point(8, 211);
-            btn_alterar.Margin = new Padding(2);
-            btn_alterar.Name = "btn_alterar";
-            btn_alterar.Size = new Size(256, 46);
-            btn_alterar.TabIndex = 5;
-            btn_alterar.Text = "Alterar";
-            btn_alterar.UseVisualStyleBackColor = true;
-            btn_alterar.Click += btn_alterar_Click;
             // 
             // Frmcategoria
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Tomato;
-            ClientSize = new Size(860, 359);
+            ClientSize = new Size(764, 479);
             Controls.Add(dgvCategoria);
             Controls.Add(groupBox1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Frmcategoria";
             Text = "Frmcategoria";
             Load += Frmcategoria_Load;
